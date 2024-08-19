ecda5fb4bdad2610a7e963d489db1cd91f2fbfa5
CaoVoThanhTong <thanhtong2407@gmail.com>
2024-08-19 21:36:14 +0700

    update

--- PhanTichDinhLuong/spiders/PhongTro123Crawler.py
+++ PhanTichDinhLuong/spiders/PhongTro123Crawler.py
@@ -7,15 +7,14 @@ class PhongTro123Spider(scrapy.Spider):
     start_urls = ['https://phongtro123.com/']
 
     def parse(self, response):
-        # Lấy danh sách các phòng trọ từ trang chủ
-        room_links = response.xpath('//div[@class="post-title"]/a/@href').getall()
+        room_links = response.xpath('//ul[@class="post-listing aside clearfix"]/li/a/@href').getall()
         for link in room_links:
             yield scrapy.Request(url=response.urljoin(link), callback=self.parse_room_detail)
 
     def parse_room_detail(self, response):
         item = PhongTroItem()
-        item['title'] = response.xpath('//h1/text()').get()
-        item['price'] = response.xpath('//span[@class="price"]/text()').get()
+        item['title'] = response.xpath('//span[@class="post-title"]/text()').get()
+        item['price'] = response.xpath('//span[@class="post-price"]/text()').get()
         item['address'] = response.xpath('//div[@class="address"]/text()').get()
         item['description'] = response.xpath('//div[@class="post-main-content"]/p/text()').getall()
         yield item
